b6e2c5cbbeab14759023b15a4430f175e2d8a43e
Nat <nat.weerawan@gmail.com>
2009-12-30 10:35:11 +0700

    bootstrap edit message to Thai

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,12 +1,12 @@
 class BootStrap {
 
      def init = { servletContext ->
-		new CongestionCause(reason:"Raining").save()
-		new CongestionCause(reason:"Accident").save()
-		new CongestionCause(reason:"Constructing").save()	
-		new CongestionLevel(level:1,info:"Low").save()
-		new CongestionLevel(level:2,info:"Moderate").save()
-		new CongestionLevel(level:3,info:"High").save()
+		new CongestionCause(reason:"อุบัติเหตุ").save()
+		new CongestionCause(reason:"ฝนตก").save()
+		new CongestionCause(reason:"ก่อสร้าง").save()	
+		new CongestionLevel(level:1,info:"คล่องตัว").save()
+		new CongestionLevel(level:2,info:"หนาแน่น").save()
+		new CongestionLevel(level:3,info:"ติดขัด").save()
 		new Role(description:"Admin Access",authority:"ROLE_ADMIN").save()
 		new Role(description:"User Access",authority:"ROLE_USER").save()
 		new Requestmap(url:"/secure/**",configAttribute:"ROLE_ADMIN").save()
