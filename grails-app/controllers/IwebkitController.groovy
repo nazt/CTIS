06e0d5d5c0aaa509fa1964d35f71ef7c4648c755
Nat <nat.weerawan@gmail.com>
2010-03-09 10:26:57 +0700

    add action : twitterlist , report

--- grails-app/controllers/IwebkitController.groovy
+++ grails-app/controllers/IwebkitController.groovy
@@ -1,3 +1,4 @@
+import grails.converters.*;
 class IwebkitController {
 
     def index = {  }
@@ -9,6 +10,7 @@ render   Report.list( params )
     }
 	def map={}
 	def view= {}
+	def about = { }
     def show = {
         def reportInstance = Report.get(params.id)
         if (!reportInstance) {
@@ -29,10 +31,28 @@ render   Report.list( params )
 		params.max = Math.min( params.max ? params.max.toInteger() : 5,  10)
 	        [ reportInstanceList: Report.list(sort:"dateCreated", order:"desc"),congestionCauseInstanceList: CongestionCause.list(), reportInstanceTotal: Report.count() ] 
 	}
+	def report={
+        def reportInstance = Report.get(params.id)
+        if (!reportInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'report.label', default: 'Report'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [reportInstance: reportInstance]
+        }
+	}	
 	def listview={
         params.max = Math.min( params.max ? params.max.toInteger() : 100,  100)
 		params.sort="createdDate"
 		params.order="desc"
         [ reportInstanceList: Report.list(sort:"dateCreated", order:"desc"), reportInstanceTotal: Report.count() ]		
+	}
+	def twitterlist={
+			def url = new URL("http://twitter.com/statuses/user_timeline.json?screen_name=ctis_tu")
+			def response = JSON.parse(url.newReader()) // response is an instance of JSONObject (see Grails API docs)
+			response.text.each {  println it   }
+/*			println response	*/
+			[ reportInstanceList:response]		
+		
 	}
 }
